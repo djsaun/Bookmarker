a0b036bc83741f145a32d9a88e9dc3110db7658e
David Saunders <david@monkee-boy.com>
2016-02-05 14:17:55 -0600

    Implemented route and controller action to search bookmarks by tag

--- config/routes.php
+++ config/routes.php
@@ -21,6 +21,34 @@
 use Cake\Core\Plugin;
 use Cake\Routing\Router;
 
+Router::defaultRouteClass('Route');
+
+// New route we're adding for our tagged action.
+// The trailing `*` tells CakePHP that this action has
+// passed parameters.
+Router::scope(
+    '/bookmarks',
+    ['controller' => 'Bookmarks'],
+    function ($routes) {
+        $routes->connect('/tagged/*', ['action' => 'tags']);
+    }
+);
+
+Router::scope('/', function ($routes) {
+    // Connect the default home and /pages/* routes.
+    $routes->connect('/', [
+        'controller' => 'Pages',
+        'action' => 'display', 'home'
+    ]);
+    $routes->connect('/pages/*', [
+        'controller' => 'Pages',
+        'action' => 'display'
+    ]);
+
+    // Connect the conventions based default routes.
+    $routes->fallbacks('InflectedRoute');
+});
+
 /**
  * The default class to use for all routes
  *
